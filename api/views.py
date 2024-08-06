257b1401dc0be6f7dfc9b6ca5b3f2c6e8c006ef5
RohanQ17 <axelb4467@gmail.com>
2024-08-06 18:32:43 +0530

    json

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 from rest_framework import status
 from .models import Job
 from .serializers import JobSerializer
@@ -9,32 +9,32 @@ def jobs_api(request, id=None):
     if request.method == 'GET':
         jobs = Job.objects.all()
         serializer = JobSerializer(jobs, many=True)
-        return Response({"jobs": serializer.data})
+        return JsonResponse({"jobs": serializer.data})
 
     elif request.method == 'POST':
         serializer = JobSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method in ['PUT', 'DELETE']:
         if id is None:
-            return Response({"error": "ID is required for PUT and DELETE operations"}, 
+            return JsonResponse({"error": "ID is required for PUT and DELETE operations"}, 
                             status=status.HTTP_400_BAD_REQUEST)
         
         try:
             job = Job.objects.get(id=id)
         except Job.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse(status=status.HTTP_404_NOT_FOUND)
 
         if request.method == 'PUT':
             serializer = JobSerializer(job, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         elif request.method == 'DELETE':
             job.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return JsonResponse(status=status.HTTP_204_NO_CONTENT)
