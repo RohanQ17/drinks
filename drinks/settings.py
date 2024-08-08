36e50481c5e77b58c133bca840c1a6d07ddeb1fc
RohanQ17 <axelb4467@gmail.com>
2024-08-08 13:48:54 +0530

    fixes

--- drinks/settings.py
+++ drinks/settings.py
@@ -62,11 +62,12 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
 }
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
      '*' # Your production domain
 ]
+Access-Control-Allow-Origin = ['*']
 
 ROOT_URLCONF = 'drinks.urls'
 
