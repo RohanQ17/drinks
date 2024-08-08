18a234f3c439de6c0caea4dfaf1962ec7d36bf7d
RohanQ17 <axelb4467@gmail.com>
2024-08-08 14:00:54 +0530

    mprefixes

--- drinks/settings.py
+++ drinks/settings.py
@@ -48,9 +48,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -62,13 +63,14 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny'],
 }
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-     '*' # Your production domain
-]
-Access-Control-Allow-Origin = ['*']
+     # Your production domain
 
+]
+
+CORS_ALLOW_CREDENTIALS = True 
 ROOT_URLCONF = 'drinks.urls'
 
 TEMPLATES = [
